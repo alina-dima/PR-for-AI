fd282a610a9231b334866e79e7172cdd46379eef
NikolaiHerrmann <nikolaiherrmann@gmail.com>
2022-09-29 17:04:16 +0200

    final script for 4

--- Assignment 1/Final/Task_04/script4.m
+++ Assignment 1/Final/Task_04/script4.m
@@ -1,11 +1,15 @@
-mu = [3 4];
-Sigma = [1 0; 0 2];
 
-x = -10:.1:10;
-y = -10:.1:10;
-[X,Y] = meshgrid(x, y);
-XY = [X(:) Y(:)];
-Z = mvnpdf(XY, mu, Sigma);
+% Task 4
+% Alina, Lisa, Ömer and Nikolai
+
+mu = [3 4];
+sigma = [1 0; 0 2];
+
+x = -10:0.1:10;
+y = -10:0.1:10;
+
+[X, Y] = meshgrid(x, y);
+Z = mvnpdf([X(:) Y(:)], mu, sigma);
 Z = reshape(Z, length(y), length(x));
 
 mesh(X, Y, Z)
@@ -17,7 +21,10 @@ colorbar
 points = [10 10; 0 0; 3 4; 6 8];
 
 for i = 1:length(points)
-   mahalanobis = mahal_dis(points(i,:)', mean', sigma);
+    fprintf("\nPoint ")
+    disp(points(i,:))
+    fprintf("Mahal Distance")
+    disp(mahal_dis(points(i,:)', mu', sigma));
 end
 
 function mahalanobis = mahal_dis(x, y, cov_mat)
