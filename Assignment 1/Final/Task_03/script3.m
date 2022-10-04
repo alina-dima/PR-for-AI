22d8b6a38b7d90e8e27e7aeced83b96a2449ba6a
Alina Dima <alina.dima99@gmail.com>
2022-10-04 14:10:55 +0200

    Task 3 with implemented means

--- Assignment 1/Final/Task_03/script3.m
+++ Assignment 1/Final/Task_03/script3.m
@@ -4,17 +4,22 @@
 
 feat_vectors = [4 5 6; 6 3 9; 8 7 3; 7 4 8; 4 6 5];
 
-means = mean(feat_vectors);
-cov_matrix = zeros(3,3);
-
-for i = 1:length(cov_matrix)
-    for j = 1:length(cov_matrix)
-        cov_matrix(i,j) = get_cov(feat_vectors(:,i), feat_vectors(:,j));
-    end
+means = zeros(1,3);
+for i = 1:size(means,2)
+    means(i) = sum(feat_vectors(:,i)) / length(feat_vectors(:,i));
 end
-
 fprintf("Mean:");
 disp(means);
+
+cov_matrix = zeros(3,3);
+for i = 1:size(cov_matrix,1)
+    for j = i:size(cov_matrix,2)
+        cov_matrix(i,j) = get_cov(feat_vectors(:,i),feat_vectors(:,j),...
+                                  means(i),means(j));
+        cov_matrix(j,i) = get_cov(feat_vectors(:,j),feat_vectors(:,i),...
+                                  means(j),means(i));
+    end
+end
 fprintf("Covariance matrix:\n");
 disp(cov_matrix);
 
@@ -23,6 +28,6 @@ y = mvnpdf(eval_points, means, cov_matrix);
 fprintf("Probabilities:\n");
 disp(y);
 
-function cov = get_cov(X,Y)
-    cov = sum((X - mean(X)).*(Y - mean(Y))) / (length(X) - 1);
+function cov = get_cov(X,Y,mu_X,mu_Y)
+    cov = sum((X - mu_X).*(Y - mu_Y)) / (length(X) - 1);
 end
