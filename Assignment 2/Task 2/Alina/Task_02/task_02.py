aa68aec2b0d0d8eecf72fe5a7383d9fa28c602c5
Lisa-dk <lisadkoopmans@gmail.com>
2022-10-27 20:41:26 +0200

    across 100 iterations

--- Assignment 2/Task 2/Alina/Task_02/task_02.py
+++ Assignment 2/Task 2/Alina/Task_02/task_02.py
@@ -1,4 +1,8 @@
+from cProfile import label
+from enum import unique
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 from sklearn.semi_supervised import LabelPropagation
 from sklearn.ensemble import RandomForestClassifier
@@ -21,15 +25,15 @@ def read_data(data_path):
     data = df.drop(columns=["Class"])
 
     # Oversamples data
-    sm = SMOTE(sampling_strategy='minority',random_state=42, k_neighbors=5)
+    sm = SMOTE(sampling_strategy='minority',random_state=42)
     data_res, labels_res = sm.fit_resample(data, labels)
 
     # Split into train and test
     x_train, x_test, y_train, y_test = train_test_split(data_res, labels_res, test_size=0.2,
-                                                        random_state=4, stratify=labels_res)
+                                                        stratify=labels_res)
     # Split train into unlabelled and labelled train sets
     x_train_unlab, x_train_lab, y_train_unlab, y_train_lab = \
-        train_test_split(x_train, y_train, test_size=0.3, random_state=4, stratify=y_train)
+        train_test_split(x_train, y_train, test_size=0.3, stratify=y_train)
     y_train_unlab = pd.Series(-1, index=y_train_unlab.index, name='Class')
 
     return x_train_unlab, y_train_unlab, x_train_lab, y_train_lab, x_test, y_test
@@ -46,6 +50,15 @@ def run_model(model, train_data, train_labels, test_data, test_labels):
     f_1 = f1_score(test_labels, yhat)
     return [accuracy, f_1], model
 
+def plot_results(accuracy, f1, title):
+    x = [i for i in range(len(f1))]
+    plt.plot(x, accuracy, label='Accuracy')
+    plt.plot(x, f1, label='F1 score')
+    plt.legend()
+    plt.title(title)
+    plt.xlabel('Iterations')
+    plt.savefig(title + '.eps', bbox_inches='tight')
+    plt.show()
 
 class Model:
   def __init__(self, model, name):
@@ -55,29 +68,45 @@ class Model:
 
 def main():
     data_path = './creditcard.csv'
-    x_train_unlab, y_train_unlab, x_train_lab, y_train_lab, x_test, y_test = read_data(data_path)
 
     # Selects the baseline classification and SSL models
     baseline = Model(RandomForestClassifier(), "Random Forest")
     ssl = Model(LabelPropagation(kernel='knn'), "KNN Label Propagation")
+    accuracy_baseline = []
+    f1_baseline = []
+    accuracy_ssl = []
+    f1_ssl = []
+    accuracy_baseline_ssl = []
+    f1_baseline_ssl = []
+    n_iter = 100
 
-    # Runs classification model on lab train data and tests it
-    print("Running baseline model on labeled data...")
-    results, _ = run_model(baseline.model, x_train_lab, y_train_lab, x_test, y_test)
-    print("Model: ", baseline.name, " \t Accuracy: %.4f \t F1: %.4f\n" % (results[0], results[1]))
+    for iter in range(n_iter):
+        x_train_unlab, y_train_unlab, x_train_lab, y_train_lab, x_test, y_test = read_data(data_path)
 
-    # Runs SSL model on all training data and tests it
-    x_train = pd.concat([x_train_lab, x_train_unlab], axis=0)
-    y_ssl = pd.concat([y_train_lab, y_train_unlab], axis=0)
-    print("Running SSL model...")
-    results, ssl_model = run_model(ssl.model, x_train, y_ssl, x_test, y_test)
-    print("Model: ", ssl.name, " \t Accuracy: %.4f \t F1: %.4f\n" % (results[0], results[1]))
+        # Runs classification model on lab train data and tests it
+        print(iter, "Running baseline model on labeled data...")
+        results, _ = run_model(baseline.model, x_train_lab, y_train_lab, x_test, y_test)
+        accuracy_baseline.append(results[0])
+        f1_baseline.append(results[1])
 
-    # Runs baseline model on all training data using the labels obtained from SSL and tests it
-    y_train_ssl = ssl_model.transduction_
-    print("Running baseline model with SSL labels on all data...")
-    results, _ = run_model(baseline.model, x_train, y_train_ssl, x_test, y_test)
-    print("Model: ", baseline.name, " \t Accuracy: %.4f \t F1: %.4f" % (results[0], results[1]))
+        # Runs SSL model on all training data and tests it
+        x_train = pd.concat([x_train_lab, x_train_unlab], axis=0)
+        y_ssl = pd.concat([y_train_lab, y_train_unlab], axis=0)
+        print("Running SSL model...")
+        results, ssl_model = run_model(ssl.model, x_train, y_ssl, x_test, y_test)
+        accuracy_ssl.append(results[0])
+        f1_ssl.append(results[1])
+
+        # Runs baseline model on all training data using the labels obtained from SSL and tests it
+        y_train_ssl = ssl_model.transduction_
+        print("Running baseline model with SSL labels on all data...")
+        results, _ = run_model(baseline.model, x_train, y_train_ssl, x_test, y_test)
+        accuracy_baseline_ssl.append(results[0])
+        f1_baseline_ssl.append(results[1])
+
+        plot_results(accuracy_baseline, f1_baseline, 'Baseline')
+        plot_results(accuracy_ssl, f1_ssl , 'Semi-supervised')
+        plot_results(accuracy_baseline_ssl, f1_baseline_ssl, 'Baseline+semi-supervised')
 
 if __name__ == "__main__":
     main()
